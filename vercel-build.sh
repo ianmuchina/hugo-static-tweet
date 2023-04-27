6ab7dc1d00a09f5050073be5756d755f133b1057
Ian Muchina <noreply@ianmuchina.com>
2023-04-28 00:37:08 +0300

    test

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "theme: hugo-static-tweet" > config.yml
 mkdir -p themes/hugo-static-tweet
-mv exampleSite/* themes/hugo-static-tweet/ 
+mv layouts/ assets/ themes/hugo-static-tweet/
+mv exampleSite/* .
 pnpm install
 pnpm exec hugo
